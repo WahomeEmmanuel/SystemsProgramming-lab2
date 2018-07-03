14e1b39eed6eae9b47736eedee6466f042f8d22e
WahomeEmmanuel <gathage.ew@gmail.com>
2018-07-03 12:07:22 +0300

    Update on user input

--- file.c
+++ file.c
@@ -3,10 +3,25 @@
 #include <fcntl.h>
 #include <string.h>
 
-void main()
+int fd;
+
+void myread()
 {
-  int fd;
-  fd = open("lab2file.txt",O_RDWR|O_CREAT);
+  char buf[100];
+  int fd1;
+  fd1 = read(fd, buf, 100);
+  if(fd1 == -1)
+  {
+    perror("Read error");
+  }
+  else{
+    printf("File read successfully \n%s\n", buf);
+  }
+}
+
+void myopen()
+{
+  fd = open("lab2file.txt",O_RDWR|O_APPEND);
   if (fd == -1)
   {
     perror("Open perror");
@@ -15,4 +30,32 @@ void main()
   {
     printf("File created successfully\n");
   }
+}
+
+void mywrite()
+{
+  char str[100];
+  printf("Enter string you want to write to a file \n");
+  scanf("%s",str);
+  write(fd,str,strlen(str));
+}
+
+void mydelete()
+{
+  int ret = unlink("delete.txt");
+  if(ret == -1)
+  {
+    perror("delete error");
+  }
+  else
+  {
+    printf("File deleted successfully.\n");
+  }
+}
+
+void main()
+{
+  myopen();
+  mywrite();
+  myread();
 }
